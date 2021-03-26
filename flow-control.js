43bb5184e3618b3a42c9577ee93380d2c1ff5a44
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-26 17:01:44 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,10 +17,10 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
-  else if (age < 13) {
+  else if (age <= 12) {
     return "You are a kid"
   }
-  else (age > 19) {
+  else (age >= 20) {
     return "You are a grownup"
   }
 }
